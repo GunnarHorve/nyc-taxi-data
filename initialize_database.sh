dcea523bd3ee79ea694e47cbedc77f6e76813fd6
GunnarHorve <gunnarhorve@gmail.com>
2018-03-10 16:56:35 -0500

    added "sudo -u postgres" b/c of bad db permissions

--- initialize_database.sh
+++ initialize_database.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-createdb nyc-taxi-data
+sudo -u postgres createdb nyc-taxi-data
 
-psql nyc-taxi-data -f create_nyc_taxi_schema.sql
+sudo -u postgres psql nyc-taxi-data -f create_nyc_taxi_schema.sql
 
-shp2pgsql -s 2263:4326 taxi_zones/taxi_zones.shp | psql -d nyc-taxi-data
-psql nyc-taxi-data -c "CREATE INDEX index_taxi_zones_on_geom ON taxi_zones USING gist (geom);"
-psql nyc-taxi-data -c "CREATE INDEX index_taxi_zones_on_locationid ON taxi_zones (locationid);"
-psql nyc-taxi-data -c "VACUUM ANALYZE taxi_zones;"
+sudo -u postgres shp2pgsql -s 2263:4326 taxi_zones/taxi_zones.shp | sudo -u postgres psql -d nyc-taxi-data
+sudo -u postgres psql nyc-taxi-data -c "CREATE INDEX index_taxi_zones_on_geom ON taxi_zones USING gist (geom);"
+sudo -u postgres psql nyc-taxi-data -c "CREATE INDEX index_taxi_zones_on_locationid ON taxi_zones (locationid);"
+sudo -u postgres psql nyc-taxi-data -c "VACUUM ANALYZE taxi_zones;"
 
-shp2pgsql -s 2263:4326 nyct2010_15b/nyct2010.shp | psql -d nyc-taxi-data
-psql nyc-taxi-data -f add_newark_airport.sql
-psql nyc-taxi-data -c "CREATE INDEX index_nyct_on_geom ON nyct2010 USING gist (geom);"
-psql nyc-taxi-data -c "CREATE INDEX index_nyct_on_ntacode ON nyct2010 (ntacode);"
-psql nyc-taxi-data -c "VACUUM ANALYZE nyct2010;"
+sudo -u postgres shp2pgsql -s 2263:4326 nyct2010_15b/nyct2010.shp | sudo -u postgres psql -d nyc-taxi-data
+sudo -u postgres psql nyc-taxi-data -f add_newark_airport.sql
+sudo -u postgres psql nyc-taxi-data -c "CREATE INDEX index_nyct_on_geom ON nyct2010 USING gist (geom);"
+sudo -u postgres psql nyc-taxi-data -c "CREATE INDEX index_nyct_on_ntacode ON nyct2010 (ntacode);"
+sudo -u postgres psql nyc-taxi-data -c "VACUUM ANALYZE nyct2010;"
 
-psql nyc-taxi-data -f add_tract_to_zone_mapping.sql
+sudo -u postgres psql nyc-taxi-data -f add_tract_to_zone_mapping.sql
 
-cat data/fhv_bases.csv | psql nyc-taxi-data -c "COPY fhv_bases FROM stdin WITH CSV HEADER;"
-cat data/central_park_weather.csv | psql nyc-taxi-data -c "COPY central_park_weather_observations FROM stdin WITH CSV HEADER;"
-psql nyc-taxi-data -c "UPDATE central_park_weather_observations SET average_wind_speed = NULL WHERE average_wind_speed = -9999;"
+cat /mnt/disks/storage/raw/fhv_bases.csv | sudo -u postgres psql nyc-taxi-data -c "COPY fhv_bases FROM stdin WITH CSV HEADER;"
+cat /mnt/disks/storage/raw/central_park_weather.csv | sudo -u postgres psql nyc-taxi-data -c "COPY central_park_weather_observations FROM stdin WITH CSV HEADER;"
+sudo -u postgres psql nyc-taxi-data -c "UPDATE central_park_weather_observations SET average_wind_speed = NULL WHERE average_wind_speed = -9999;"
